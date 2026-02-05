3311a79206aecba246885a5e2ad30728c0cfb4b9
rchavhan276 <rchavhan276@gmail.com>
2026-02-05 12:16:18 +0530

    adding the projects

---
+++ dbo/Tables/EmployMaster.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[EmployMaster] (
+    [EmployID]  INT          NOT NULL,
+    [FirstName] VARCHAR (50) NOT NULL,
+    [LastName]  VARCHAR (50) NOT NULL,
+    [City]      VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([EmployID] ASC)
+);
+
