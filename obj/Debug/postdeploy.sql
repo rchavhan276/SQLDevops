daf34a84d39ecb1a80375ab20708fff7471e3273
rchavhan276 <rchavhan276@gmail.com>
2026-02-06 15:49:01 +0530

    adding data also

---
+++ obj/Debug/postdeploy.sql
@@ -0,0 +1,35 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+PRINT 'Seeding EmployMaster data';
+
+MERGE dbo.EmployMaster AS T
+USING (VALUES
+    (101, N'Martin', N'warner', NULL, NULL, NULL),
+    (102, N'Rohit',  N'sharma', NULL, NULL, NULL),
+    (103, N'cris',   N'gayle',  NULL, NULL, NULL)
+) AS S (EmployID, FirstName, LastName, City, mobile, age)
+
+ON T.EmployID = S.EmployID
+
+WHEN MATCHED THEN
+    UPDATE SET
+        FirstName = S.FirstName,
+        LastName  = S.LastName,
+        City      = S.City,
+        mobile    = S.mobile,
+        age       = S.age
+
+WHEN NOT MATCHED THEN
+    INSERT (EmployID, FirstName, LastName, City, mobile, age)
+    VALUES (S.EmployID, S.FirstName, S.LastName, S.City, S.mobile, S.age);
+
+GO
