31ab1395179c0ae9c1fc3013b2e3a251c326bea4
rchavhan276 <rchavhan276@gmail.com>
2026-02-06 16:46:52 +0530

    adding mobile number

--- Script.PostDeployment1.sql
+++ Script.PostDeployment1.sql
@@ -13,22 +13,24 @@ PRINT 'Seeding EmployMaster data';
 
 MERGE dbo.EmployMaster AS T
 USING (VALUES
-    (101, N'Martin', N'warner', NULL, NULL, NULL),
-    (102, N'Rohit',  N'sharma', NULL, NULL, NULL),
-    (103, N'cris',   N'gayle',  NULL, NULL, NULL)
-) AS S (EmployID, FirstName, LastName, City, mobile, age)
+    (101, N'Martin', N'warner', NULL, 513661666, NULL, NULL),
+    (102, N'Rohit', N'sharma', NULL, NULL, NULL, NULL),
+    (103, N'cris', N'gayle', NULL, NULL, NULL, NULL)
+) AS S (EmployID, FirstName, LastName, City, mobile, age, Mobile_Number)
 
 ON T.EmployID = S.EmployID
 
 WHEN MATCHED THEN
     UPDATE SET
-        FirstName = S.FirstName,
-        LastName  = S.LastName,
-        City      = S.City,
-        mobile    = S.mobile,
-        age       = S.age
+        FirstName     = S.FirstName,
+        LastName      = S.LastName,
+        City          = S.City,
+        mobile        = S.mobile,
+        age           = S.age,
+        Mobile_Number = S.Mobile_Number
 
 WHEN NOT MATCHED THEN
-    INSERT (EmployID, FirstName, LastName, City, mobile, age)
-    VALUES (S.EmployID, S.FirstName, S.LastName, S.City, S.mobile, S.age);
+    INSERT (EmployID, FirstName, LastName, City, mobile, age, Mobile_Number)
+    VALUES (S.EmployID, S.FirstName, S.LastName, S.City, S.mobile, S.age, S.Mobile_Number);
+
 
